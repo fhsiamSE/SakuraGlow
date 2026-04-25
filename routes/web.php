1c3c1092d89d89bf5ef5c8474f8e36fe8d9e1e9c
fhsiamSE <faisalhossainsiam@gmail.com>
2026-04-25 20:37:41 +0600

    added middleware

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Sale;
 use App\Http\Controllers\Transaction;
 use App\Http\Controllers\Stock;
 use App\Http\Controllers\DashboardController;
+use App\Http\Middleware\ValidUser;
 Route::get('/', function () {
     return  view('login');
 })->name('login');
@@ -17,9 +18,15 @@ Route::get('/registration', function () {
 Route::post('/register', [App\Http\Controllers\AdminController::class, 'register'])->name('admin.register');
 Route::post('/login', [App\Http\Controllers\AdminController::class, 'authCheck'])->name('authCheck');
 
+
+Route::middleware(ValidUser::class)->group(function(){
+
 Route::get('/dashboard', [DashboardController::class, 'show'])->name('Dashboard');
 Route::Resource('products', App\Http\Controllers\ProductController::class);
 Route::Resource('sales', App\Http\Controllers\SaleController::class);
 Route::Resource('transactions', App\Http\Controllers\TransactionController::class);
 Route::resource('stocks', App\Http\Controllers\StockController::class);
 Route::resource('categories', App\Http\Controllers\CategorieController::class);
+Route::get('/logout', [App\Http\Controllers\AdminController::class, 'logout'])->name('logout');
+
+});
